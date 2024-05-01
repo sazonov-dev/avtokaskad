fd0197001c016d1331851b04ba140fc74c68f529
888Cruiser <nsazonov888@gmail.com>
2024-05-02 01:04:15 +0300

    header desktop

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './scss/styles.scss';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const gallery = document.querySelector('.header__content-gallery-scroll');
+  const scrollAmount = 100;
+
+  function handleScroll(event) {
+    event.preventDefault();
+    const delta = event.deltaY || event.detail || event.wheelDelta;
+    gallery.scrollLeft += Math.sign(delta) * scrollAmount;
+  }
+
+  gallery.addEventListener('wheel', handleScroll);
+  gallery.addEventListener('DOMMouseScroll', handleScroll);
+});
