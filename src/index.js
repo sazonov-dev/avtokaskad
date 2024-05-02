240c8ff938c1db3785e5cb7bdf0d94ed058aca0e
Nikita Sazonov <proger@MacBook-Pro-Nikita.local>
2024-05-02 16:51:07 +0300

    added aos & about section desktop

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import './scss/styles.scss';
 
 document.addEventListener('DOMContentLoaded', function () {
   const gallery = document.querySelector('.header__content-gallery-scroll');
-  const scrollAmount = 100;
 
-  function handleScroll(event) {
-    event.preventDefault();
-    const delta = event.deltaY || event.detail || event.wheelDelta;
-    gallery.scrollLeft += Math.sign(delta) * scrollAmount;
+  function handleWheel(event) {
+    if (event.deltaY !== 0 && Math.abs(event.deltaX) < Math.abs(event.deltaY)) {
+      event.preventDefault();
+      gallery.scrollLeft += event.deltaY;
+    }
   }
 
-  gallery.addEventListener('wheel', handleScroll);
-  gallery.addEventListener('DOMMouseScroll', handleScroll);
+  gallery.addEventListener('wheel', handleWheel);
 });
+
