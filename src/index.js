374750cdec5f7837ff4ebf2055299f109ad72c4b
Nikita Sazonov <proger@MacBook-Pro-Nikita.local>
2024-05-07 14:54:23 +0300

    added form validation and toast notification

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './scss/styles.scss';
 
 document.addEventListener('DOMContentLoaded', function () {
   const gallery = document.querySelector('.header__content-gallery-scroll');
-
   function handleWheel(event) {
     if (event.deltaY !== 0 && Math.abs(event.deltaX) < Math.abs(event.deltaY)) {
       event.preventDefault();
